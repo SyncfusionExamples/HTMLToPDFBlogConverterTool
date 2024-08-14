77de12f63f8aa57a93e5c02addca308412881587
Karmegam <103259299+karmegams02@users.noreply.github.com>
2024-08-14 17:41:26 +0530

    Added the Blog to PDF converter sample using the HTMLConverter library

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTMLToPDF_WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/URL.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HTMLToPDF_WebApplication.Models
+{
+    public class URL
+    {
+        [Required]
+        public string AdURL { get; set; }
+        [Required]
+        public string ImageURL { get; set; }
+        [Required]
+        public string HeaderText { get; set; }
+        [Required]
+        public string BlogLink { get; set; }
+    }
+}
