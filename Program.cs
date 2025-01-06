9d18098ab23424f3294a885b96b28d53dc3037d4
SathishSuresh09 <sathish.suresh@syncfusion.com>
2025-01-06 10:35:58 +0530

    htmltopdf

--- Program.cs
+++ Program.cs
@@ -11,7 +11,27 @@ namespace HTMLToPDF_WebApplication
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            // Configure cookie settings
+            builder.Services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.Secure = CookieSecurePolicy.Always; 
+                options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always;
+            });
+            builder.Services.AddAntiforgery(options =>
+            {
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always; 
+                options.Cookie.HttpOnly = true; 
+                options.Cookie.SameSite = SameSiteMode.Strict; 
+            });
             var app = builder.Build();
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("Content-Security-Policy", "upgrade-insecure-requests; script-src 'self' 'unsafe-inline' 'unsafe-eval'  *.syncfusion.com; style-src 'self' 'unsafe-inline'  *.syncfusion.com; frame-src  *.syncfusion.com; object-src 'none'; base-uri 'none'; default-src 'self'; font-src 'self' *.syncfusion.com; connect-src 'self'  *.syncfusion.com; img-src 'self' *.syncfusion.com; form-action 'self'; frame-ancestors 'self'");
+
+                await next();
+            });
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
